0d63cee2d04900e848143402fe885a22f79e78ea
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2023-11-22 21:07:21 +0100

    Update main.c

--- Programmazione1/ES_04_sommaSelettiva/main.c
+++ Programmazione1/ES_04_sommaSelettiva/main.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
-#include <math.h>
-#define MAX_N 20
-
+#include "math.h"
+#define MAX_N 20 
 int main() {
-
-    int dim = -1;
-    while(dim==-1 || dim>MAX_N){
-        scanf("%d",&dim);
-    }
-    if(dim!=0){
-        int array[dim];
-        int minimo=0;
-        int somma=0;
-        for(int i=0;i<dim;i++){
-            scanf("%d",&array[i]);
-            if(i==0){
-                minimo = array[i];
-            }
-            if(array[i]<minimo){
-                minimo = array[i];
-            }
-        }
-        for(int i=0;i<dim;i++){
-            if(array[i]>= pow(minimo,2)){
-                somma += array[i];
-            }
-        }
-        printf("%d",somma);
+    
+    int sequenza=-1;
+    scanf("%d",&sequenza);
+    
+    if(sequenza<0 || sequenza > MAX_N){
+        printf("Errore: numero di elementi non valido.");
     }
     else{
-        printf("Vuoto.");
+        int elementi[sequenza];
+        if(sequenza!=0){
+            for(int i=0; i<sequenza; i++){
+                scanf("%d", &elementi[i]);
+            } 
+            int minimo=elementi[0];
+            for(int i=1; i<sequenza; i++){
+                if(elementi[i]<minimo){
+                minimo=elementi[i];
+                }
+            }
+            int somma=0;
+            for(int i=0; i<sequenza; i++){
+                if(elementi[i] >= pow(minimo,2)){
+                somma+=elementi[i];
+                }
+            }printf("%d", somma);
+        } 
+        else {
+            printf("Vuoto.");
+        }   
     }
     return 0;
 }
