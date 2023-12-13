0cdacb40350b858377c4e721f1e82f203390725b
Davide Ristorto <ristorto.davide@gmail.com>
2023-12-13 08:41:23 +0100

    Ricerca dicotomica

--- Programmazione1/ES_18_RicercaBinaria/main.c
+++ Programmazione1/ES_18_RicercaBinaria/main.c
@@ -2,21 +2,20 @@
 #include <stdbool.h>
 
 bool ricerca_binaria(const int a[], const int val, size_t *pIndice,
-                const size_t left, const size_t right){
-    if(left>=right){
+                     const size_t left, const size_t right) {
+    if (left >= right) {
         return false;
-    }
-    else{
-        size_t mid = (left + right) / 2;
-        if (a[mid] == val){
-            *pIndice = mid;
+    } else {
+        size_t mid = left + (right - left) / 2;
+        *pIndice = mid;
+        if (a[mid] == val) {
             return true;
         } else {
-          if(val>a[mid]){
-              return ricerca_binaria(a, val, pIndice, mid+1, right);
-          }else{
-              return ricerca_binaria(a, val, pIndice, left, right);
-          }
+            if (val > a[mid]) {
+                return ricerca_binaria(a, val, pIndice, mid + 1, right);
+            } else {
+                return ricerca_binaria(a, val, pIndice, left, mid);
+            }
         }
     }
 }
