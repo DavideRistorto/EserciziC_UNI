64873e8dcecd47c52d8f2391238ef63a73ad5adc
Davide Ristorto <ristorto.davide@gmail.com>
2024-03-05 11:51:57 +0100

    Lab1B funzionante

--- Programmazione2/Lab1B/main.c
+++ Programmazione2/Lab1B/main.c
@@ -15,32 +15,45 @@
  *           Non si accorge dell'eventuale overflow (restituendo in questo caso un risultato tipicamente scorretto).
  */
 #include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
 
 int stoub(char *s, unsigned short b, int *r) {
-    if(b < 2 || b > 36){
+    //se la base non appartiene al range o la stringa da convertire è nulla, uscita dal programma
+    if (b < 2 || b > 36 || strlen(s)==0) {
         *r = 0;
         return 0;
     }
     int result = 0;
-    for(int i=0; s[i] != '\0'; i++){
-        if((isspace(*s))){
-            i++;
+    int exponent = 0;
+    bool negative = false;
+    //scorriamo la stringa dalla fine verso l' inizio
+    for (int i = strlen(s)-1; i>=0 ; i--) {
+        if(s[i] == '-'){
+            negative = true;
         }
-        if(!isalpha(s[i]) && !isdigit(s[i])){
-            return 0;
+        //controlliamo che il carattere attuale sia un numero/lettera
+        if(isalpha(s[i]) || isdigit(s[i])){
+           //fase di coversione nella rispettiva base
+           // se il carattere è una cifra prendiamo il suo val codice ASCII e lo sottraiamo col valore ASCII del carattere '0'
+           // se il carattere è una lettera, sottraiamo il suo valore a quello della lettera A, aggiunto di 10, perche A=10
+            result += (isdigit(s[i]) ? s[i] - '0' : toupper(s[i]) - 'A' + 10) * pow(b, exponent);
+            exponent++;
         }
-        int digit;
-        if (isdigit(*s))
-            digit = *s - '0';
-        else
-            digit = toupper(*s) - 'A' + 10;
-
-        if (digit >= b)  // Carattere non valido per la base
-            return 0;
-
-        result = result * b + digit;
-        s++;
     }
+    //short if per controllo del segno
+    //Esempio: variable = (condition) ? expressionTrue : expressionFalse;
+    result = negative ? -result : result;
     *r = result;
     return 1;
 }
+
+/*per testare il programma a mano
+int main() {
+    int ciao;
+    int* result = &ciao;
+    int res = stoub("z",16,result);
+    printf("%d %d", res, *result);
+}
+*/
