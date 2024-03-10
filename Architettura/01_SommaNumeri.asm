cc3347fcf2cc583cf9f1eb3bed5a0dc686df9a6c
Davide Ristorto <ristorto.davide@gmail.com>
2024-03-10 12:19:45 +0100

    esercizi assembly

---
+++ Architettura/01_SommaNumeri.asm
@@ -0,0 +1,16 @@
+.globl _start
+
+.text
+
+_start:
+
+    addi x5, x0, 41
+    addi x6, x0, 43
+    addi x7, x0 , 47
+    add x28, x5, x6
+    add x28, x28, x7
+
+
+exit:
+    addi x17, x0, 10  # call number 10 = exit
+    ecall
