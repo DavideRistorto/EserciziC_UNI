5681d4391b48df1eebe71b8db56fbfb13dc0b51c
Davide Ristorto <ristorto.davide@gmail.com>
2024-03-24 23:54:23 +0100

    Update main.c

--- Programmazione2/Lab04/main.c
+++ Programmazione2/Lab04/main.c
@@ -27,7 +27,6 @@ void printQueue(CharQueueADT q) {
 
 /* @brief Restituisce una coda vuota, se non trova memoria restituisce NULL */
 CharQueueADT mkQueue() {
-    printf("MALLOC\n");
     CharQueueADT queue = (CharQueueADT)malloc(sizeof(struct charQueue));
     if(queue != NULL){
         queue->front = NULL;
@@ -58,7 +57,7 @@ _Bool enqueue(CharQueueADT q, const char e) {
         q->front = newNode;
     }
     q->rear = newNode;
-    printQueue(q);
+    //printQueue(q);
     return 1;
 }
 
@@ -72,8 +71,15 @@ _Bool dequeue(CharQueueADT q, char* res) {
         return 0;
     }else{
         printf("NON NULL\n");
-        res = &(q->front->data);
-        return q->front->data;
+        ListNodePtr newNode = q->front;
+        *res = q->front->data;
+        q->front = q->front->next;
+        if(q->front == NULL){
+            q->rear = NULL;
+        }
+        free(newNode);
+        newNode = NULL;
+        return 1;
     }
 }
 
@@ -81,17 +87,36 @@ _Bool dequeue(CharQueueADT q, char* res) {
 
 /* @brief Controlla se la coda è vuota */
 _Bool isEmpty(CharQueueADT q) {
+    if(q != NULL && q->front != NULL && q->rear != NULL){
+        return 0;
+    }
    return 1;
 }
 
 /* @brief Restituisce il numero degli elementi presenti nella coda */
 int size(CharQueueADT q) {
-    return 1;
+    int counter = 0;
+    ListNodePtr node = q->front;
+    while(node != NULL){
+        counter++;
+        node = node->next;
+    }
+    return counter;
 }
 
 /* @brief Restituisce l'elemento nella posizione data (senza toglierlo) */
+//--------------------TODO: Implementare peek
 _Bool peek(CharQueueADT q, int position, char *res) {
+    int counter = 0;
+    ListNodePtr node = q->front;
+    while(node->next != NULL && counter < position){
+        counter++;
+        node = node->next;
+        printf("Counter: %d  Letter:%c\n", counter, node->data);
+    }
+    if(node == NULL){
+        res = NULL;
+        return 0;
+    }
     return 1;
 }
-
-
