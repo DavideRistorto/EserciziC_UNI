f0782f7eceb60af57cc68c90138421a9a024ddd5
Davide Ristorto <ristorto.davide@gmail.com>
2024-04-06 12:23:28 +0200

    commit

--- Architettura/EserciziQuiz/Es07_25Marzo.asm
+++ Architettura/EserciziQuiz/Es07_25Marzo.asm
@@ -1,36 +1,50 @@
 .globl _start
 .data
-    size:   .word  8                  # size of the array        
-    array:  .dword 1,2,3,4,5,6,7,8    # array of long integers
+  size:   .word 16
+  array:  .dword 12, 2, 1, 3, 5, 1, 7, 1, -1, 4, -2, -3, 1, 9, -6, 10
+newline:  .string " "
 
 .text
 _start:
-    # completare con il codice assembly RISC-V nel campo sottostante
-la t0, size     #size address
-lw t0, 0(t0)    #size value
-la t1, array    #indirizzo array
 
-# t4 consiene l'indirizzo dell' ultimo elemento
-addi a0, a0, 8  #8 di appoggio
-mul t4, t0, a0 # t4 = size*8
-addi t4, t4, -8 # t4 -= 8   
-addi a1, a1, 2
-div a1, t0, a1 #contatore del ciclo
+la t0, size
+lw t0, 0(t0)				#size	
+li t1, 8					#temp 8
+mul t1, t1,t0			#t1 = 8*size
+addi t1, t1, -8			#t1 = (size*8) - 8 per avere l' ultimo indice
+la t2, array				#t2 = indirizzo array
+mv t4, t2				#t4 sarà l'indice iniziale
+add t2, t2, t1			#t2 sarà l'indice finale
+srai t0, t0, 1
 
 beq t0, zero, FINE_CICLO
-CICLO:
-    lw t2, 0(t0)    #elem1
-    lw t5, 0(t4)    #elem2
-    mv t3, t2       # temp = elem1
-    mv t2, t5       #elem1 = elem2
-    mv t5, t3       #elem2 = elem1
-    sw t2, 0(t0)    #aggiorno array
-    sw t5, 0(t4)    #aggiorno array
-    addi t0, t0, 8  #passo ad elem successivo
-    addi t4, t4,-8  #passo ad elemento precedente
-    addi t0, t0, -1
-    bne t0, a1, CICLO
-FINE_CICLO:
 
-        
-    
+CICLO:
+	lw t5, 0(t4)			#carico primo elem array
+	lw t3, 0(t2)			#carico ultimo elem array
+	add t6, zero, t5	# t6->temp = primo
+	sw t3, 0(t4)			#array[primo] = ultimo
+	sw t6, 0(t2)			#array[ultimo]  = primo
+	addi t0, t0, -1		#decremento size
+	addi t4, t4, 8
+	addi t2, t2, -8
+	bne t0, zero, CICLO	
+FINE_CICLO:	
+
+la t0, size
+lw t0, 0(t0)
+la t1, array
+
+CICLO2:
+	lw t2, 0(t1)
+	# stampo elemento
+	li a7, 1
+	mv a0, t2
+	ecall
+		la a0, newline        #print newline
+	li a7, 4
+	ecall
+	addi t0, t0, -1
+	addi t1, t1, 8
+	bne t0, zero, CICLO2
+
