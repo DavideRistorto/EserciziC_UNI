b2b5aac33ec89b373b2a293730ab9ee9e39cd3eb
Davide Ristorto <ristorto.davide@gmail.com>
2024-04-13 16:07:25 +0200

    Update ES_01.asm

--- Architettura/EserciziQuiz8Aprile/ES_01.asm
+++ Architettura/EserciziQuiz8Aprile/ES_01.asm
@@ -20,14 +20,14 @@ _start:
 
 charAt:
     li t0, 0            # Inizializza il contatore i = 0
-    lw t2, 0(a0)        # Carica il primo carattere
+    lb t2, 0(a0)        # Carica il primo carattere
 CICLO:
     beq t0, a1, FINECICLO  # Se i == lunghezza, esce dal ciclo
     addi a0, a0, 1      # Passa al carattere successivo
     addi t0, t0, 1      # Incrementa il contatore i
-    lw t2, 0(a0)        # Carica il carattere successivo
+    lb t2, 0(a0)        # Carica il carattere successivo
     j CICLO             # Ritorna all'inizio del ciclo
 
 FINECICLO:
     mv a0, t2           # Restituisce il carattere
-    jr ra               # Ritorna al chiamante
+    ret               		# Ritorna al chiamante
