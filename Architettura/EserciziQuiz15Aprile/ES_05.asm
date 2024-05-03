c6a8d1cf88f37420088f1f4c8e0ef0b34becf72e
Davide Ristorto <ristorto.davide@gmail.com>
2024-05-03 17:54:01 +0200

    es 5

---
+++ Architettura/EserciziQuiz15Aprile/ES_05.asm
@@ -0,0 +1,14 @@
+mcm:
+    addi sp, sp, -16
+    sd s1, 8(sp)
+    sd ra, 0(sp)
+    
+    mul s1, a0, a1
+    jal mcd
+    div a0, s1, a0
+    ld s1, 8(sp)
+    ld ra, 0(sp)
+    addi sp, sp, 16
+    ret
+    
+    ret
