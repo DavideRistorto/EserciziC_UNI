278e7c2655a21686373c9583e73c65132ef7c8fb
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2024-05-18 14:03:45 +0200

    Update main.c

--- Programmazione2/Lab04/main.c
+++ Programmazione2/Lab04/main.c
@@ -54,22 +54,21 @@ _Bool dequeue(CharQueueADT q, char* res) {
     // se la coda è vuoto non posso fare la dequeue
     if(q->front == NULL){
         return 0;
-    }else{
-        // nodo temporaneo che punta al primo nodo della coda
-        ListNodePtr newNode = q->front;
-        // estraggo il risultato
-        *res = q->front->data;
-        // aggiorno la coda che partirà dal secondo nodo
-        q->front = q->front->next;
-        // nel caso la coda fosse formata da un solo nodo,
-        // la coda sarà vuota e il reaa/front saranno nulli
-        if(q->front == NULL){
-            q->rear = NULL;
-        }
-        free(newNode);
-        q->size--;
-        return 1;
     }
+    // nodo temporaneo che punta al primo nodo della coda
+    ListNodePtr newNode = q->front;
+    // estraggo il risultato
+    *res = q->front->data;
+    // aggiorno la coda che partirà dal secondo nodo
+    q->front = q->front->next;
+    // nel caso la coda fosse formata da un solo nodo,
+    // la coda sarà vuota e il reaa/front saranno nulli
+    if(q->front == NULL){
+        q->rear = NULL;
+    }
+    free(newNode);
+    q->size--;
+    return 1;
 }
 
 _Bool isEmpty(CharQueueADT q) {
