ec320b9a3305748ddcf9b99ee767b29b14a6d45e
Davide Ristorto <ristorto.davide@gmail.com>
2024-05-27 22:20:37 +0200

    Update contactBook.c

--- Programmazione2/Lab09/contactBook.c
+++ Programmazione2/Lab09/contactBook.c
@@ -1,24 +1,21 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-
 #include "contactBook.h"
 #include "sortedSetADT.h"
+
 
 /**
  Un tipo di dato per una rubrica di contatti
 */
 struct contactBookADT {
-  SortedSetADTptr contacts; // Tutto quello che serve è già in questa struttura
+    SortedSetADTptr contacts; // Tutto quello che serve è già in questa struttura
 };
 
 void stampaContact(void* elem) {
-  ContactPtr contact = (ContactPtr)elem;
-  printf("%s %s: %s (%s)", getName(contact), getSurname(contact), getMobile(contact), getUrl(contact));
+    ContactPtr contact = (ContactPtr)elem;
+    printf("%s %s: %s (%s)", getName(contact), getSurname(contact), getMobile(contact), getUrl(contact));
 }
 
 int contact_cmp(void* c1, void* c2) {
-  return cmpContact((ContactPtr)c1, (ContactPtr)c2);
+    return cmpContact((ContactPtr)c1, (ContactPtr)c2);
 }
 
 // restituisce una rubrica vuota, NULL se errore
@@ -32,8 +29,10 @@ ContactBookADTptr mkCBook() {
 
 // distrugge la rubrica, recuperando la memoria, false se errore
 _Bool dsCBook(ContactBookADTptr* book) {
-  if( *book == NULL || book == NULL)  return false;
-
+  if(book == NULL || *book == NULL){
+    return 0;
+  }  
+  //distruggo l' albero
   dsSSet(&((*book)->contacts));
   free(*book);
   *book = NULL;
@@ -56,11 +55,17 @@ int cbook_size(const ContactBookADT* book) {
 
 // aggiunge un contatto alla rubrica (restituisce false se l'elemento era gia' presente, true altrimenti)
 _Bool cbook_add(ContactBookADTptr book, ContactPtr cnt) {
+    if(book == NULL){
+        return 0;
+    }
   return sset_add(book->contacts, cnt);
 }
 
 // toglie un elemento all'insieme (restituisce false se l'elemento non era presente, true altrimenti)
 _Bool cbook_remove(ContactBookADTptr book, char* name, char* surname) {
+    if(book == NULL){
+        return 0;
+    }
   ContactPtr contact = mkContact(name, surname, "", "");
   return sset_remove(book->contacts, contact);
 }
@@ -71,45 +76,45 @@ ContactPtr cbook_search(const ContactBookADT* book, char* name, char* surname) {
   return sset_search(book->contacts, contact);
 }
 
-// carica una rubrica da file, NULL se errore
 ContactBookADTptr cbook_load(FILE* fin) {
-  //nuova rubirca
-  ContactBookADTptr book = mkCBook();
-  // apro file in lettura
-  fin = fopen("temp.csv","r");
-  //file errore o rubrica con allocata
-  if(fin == NULL || book == NULL){
-    return NULL;
-  }
-  //read the file and for each line create a contact and add it to the book
-  char line[1024];
-  char name[100], surname[100], mobile[100], url[100];
-  while(fgets(line,sizeof(line),fin)){
-    sscanf(line, "%[^,],%[^,],%[^,],%[^\n]", name, surname, mobile, url);
-    ContactPtr contact = mkContact(name, surname, mobile, url);
-    cbook_add(book, contact);
-  }
-  return book;
+    ContactBookADTptr book = mkCBook();
+    if (fin == NULL || book == NULL) {
+        return NULL;
+    }
+    char line[1024];
+    char name[100], surname[100], mobile[100], url[100];
+    while (fgets(line, sizeof(line), fin)) {
+        int numFields = sscanf(line, "%99[^,],%99[^,],%99[^,],%99[^\n]", name, surname, mobile, url);
+        if (numFields >= 2) {  // At least name and surname are required
+            if (numFields < 3) strcpy(mobile, "");
+            if (numFields < 4) strcpy(url, "");
+            ContactPtr contact = mkContact(name, surname, mobile, url);
+            printf("%s,%s,%s,%s\n", getName(contact), getSurname(contact), getMobile(contact), getUrl(contact));
+            cbook_add(book, contact);
+        }
+    }
+    return book;
 }
 
-// salva una rubrica su file, false se errore
+//salva una rubrica su file, false se errore
 _Bool cbook_dump(const ContactBookADT* book, FILE* fout) {
-  // apro file in scrittura
-  fout = fopen("temp.csv","w");
-  //file errore
-  if(fout == NULL){
-    return false;
-  }
-  //se la rubrica è vuota pulisco il file
-  if(book == NULL || sset_size(book->contacts) > 0){
-    fclose(fout);
-    return 1;
-  }
-  void** array = sset_toArray(book->contacts);
-  for(int i=0; i<sset_size(book->contacts); i++){
-    ContactPtr contact = array[i];
-    fprintf(fout, "%s,%s,%s,%s\n", getName(contact), getSurname(contact), getMobile(contact), getUrl(contact));
-  }
-  fclose(fout);
-  return 1;
+    if (book == NULL || fout == NULL) {
+        return false;
+    }
+    int size = cbook_size(book);
+    if (size == 0) {
+        // Se la rubrica è vuota, non c'è nulla da salvare
+        return true;
+    }
+    void** array = sset_toArray(book->contacts);
+    for (int i = 0; i < size; i++) {
+        ContactPtr contact = array[i];
+        if (contact != NULL) {
+            printf("DEBUG: Writing contact - Name: %s, Surname: %s, Mobile: %s, URL: %s\n",
+                   getName(contact), getSurname(contact), getMobile(contact), getUrl(contact));
+            fprintf(fout, "%s,%s,%s,%s\n", getName(contact), getSurname(contact), getMobile(contact), getUrl(contact));
+        }
+    }
+    free(array);
+    return true;
 }
