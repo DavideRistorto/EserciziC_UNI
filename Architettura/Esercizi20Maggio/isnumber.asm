043f40dc0c6932637a9aab3e87b62eea91eee4a4
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2024-06-18 18:34:54 +0200

    Update isnumber.asm

--- Architettura/Esercizi20Maggio/isnumber.asm
+++ Architettura/Esercizi20Maggio/isnumber.asm
@@ -13,18 +13,18 @@ _start:
     ecall
 
 #**************************************************************
-# completare la isnumber qua sotto (controlla se una stringa è un numero)
+# completare la isnumber qua sotto (controlla se una stringa Ã¨ un numero)
 isnumber:
 	li t0, 48 	#carattere '0'
 	li t1, 57	#carattere '9'
 	li t3, 1	#valore true a default
 	
 WHILE:
-	lb t2, 0(a0)		#leggo il carattere
+	lbu t2, 0(a0)		#leggo il carattere
 	beq t2, zero, END_WHILE
 	blt t2, t0, NO_CIFRA
 	bgt t2, t1, NO_CIFRA
-	addi a0, a0,1	#incremento il carattere
+	addi a0, a0, 1	#incremento il carattere
 	j WHILE
 	NO_CIFRA:		#return false
 		li t3, 0
