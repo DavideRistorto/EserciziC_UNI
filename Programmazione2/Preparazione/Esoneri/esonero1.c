5f0d826348f2892946881171c77e39de8b705fb5
Davide Ristorto <ristorto.davide@gmail.com>
2024-06-27 11:44:22 +0200

    Update esonero1.c

--- Programmazione2/Preparazione/Esoneri/esonero1.c
+++ Programmazione2/Preparazione/Esoneri/esonero1.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
 
 /**
 *@brief data una stringa in input, restituirne una nuova senza cifra e se ci sono 'a' o 'b'
@@ -8,33 +7,9 @@
 *
 */
 char* change(char* str){
-	//caso stringa di input non valida
-	if(str == NULL){
-		return NULL;
-	}
-	//alloco lo spazio per la nuova stringa
-	char* conversion = malloc(100*sizeof(char));
-	//memorizzo l'indice della stringa di input
-	int i=0;
-	//memorizzo l'indice della nuova stringa, perchè può essere diverso da 'i'
-	int j=0;
-	//scorro tutta la stringa fino alla sua fine
-	while(str[i] != '\0'){
-		//se è una cifra, non metto nulla nella nuova stringa
-		if(!isdigit(str[i])){
-			//se e a o b ci metto il loro valore maiuscolo
-			if(str[i] == 'a' || str[i] == 'b'){
-				conversion[j] = toupper(str[i]);
-			}else{
-			conversion[j] = str[i];
-			}
-			j++;
-		}
-		i++;
-	}
-	conversion[j] = '\0';
-	return conversion;
+	
 }
+	
 
 int main(){
 	// la stringa iniziale non posso alloracarla con la malloc perchè è una stringa costante
