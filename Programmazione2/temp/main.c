d434316cb51a4dad6c18835b4cd7e57986a09f1d
Davide Ristorto <ristorto.davide@gmail.com>
2024-07-07 11:03:36 +0200

    commit

--- Programmazione2/temp/main.c
+++ Programmazione2/temp/main.c
@@ -12,6 +12,15 @@ int main() {
   int x = 5;
   int* y = &x;
   //NULL NON DA ERRORE PERCHE' NULL E' UN PUNTATORE
-  func(x,NULL);
+  //func(x,NULL);
+
+  int a[5] = {1, 2, 3, 4, 5};
+  int b[5] = a;
+ if(a!=b){
+    printf("a e b sono diversi\n");
+  }else{
+    printf("a e b sono uguali\n");
+ }
+  
   return 0;
 }
