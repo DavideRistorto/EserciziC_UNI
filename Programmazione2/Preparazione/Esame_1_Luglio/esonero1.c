372987d415a4cb9811ac6299eea1ef668792ba9f
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2024-09-05 15:48:43 +0200

    Update esonero1.c

--- Programmazione2/Preparazione/Esame_1_Luglio/esonero1.c
+++ Programmazione2/Preparazione/Esame_1_Luglio/esonero1.c
@@ -2,42 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*  INSERIRE QUI EVENTUALI FUNZIONI AUSILIARIE (CON UN COMMENTO SIGNIFICATIVO IN TESTA) */
-
 /**
- * @brief Date una stringa s e due indici i, j validi per s (cioè tali da fare riferimento
- * a caratteri della stringa), restituisce una nuova stringa che nella posizione 0 contiene s[j],
- * nella posizione 1 contiene  s[j-1], eccetera terminando con i caratteri trovati nelle
- * posizioni s[i+1], s[i] (se i > j, restituisce la stringa vuota). Si noti che nella stringa
- * risultato i caratteri sono riportati in ordine inverso rispetto alla stringa originaria.
+ * @brief Date una stringa s, la sua lunghezza n >= 0, e un numero m tale che m >= 0,
+ * restituisce la stringa che, nella posizione 0, contiene s[n-1]; nella posizione 1,
+ * contiene  s[n-2]; eccetera, terminando con i caratteri trovati nelle posizioni
+ * s[n-min(n,m)+1], s[n-min(n,m)] (se m == 0, restituisce la stringa vuota).
+ * Si noti che nella stringa risultato i caratteri sono riportati in ordine inverso
+ * rispetto alla stringa originaria.
  *
  * ESEMPI: 
- * (1) dati "eccocosafare", 4, 7 restituisce “asoc”.
- * (2) dati "eccocosafare”, 8,3 restituisce “”.
+ * (1) dati "eccocosafare", 12, 5 restituisce "erafa".
+ * (2) dati "eccocosafare", 12,0 restituisce "".
  */
-char *extract(char *s, int i, int j){
-	if(i>j){
-		return ""; //ritorno la stringa vuota
+ 
+char *tailextract(char *s, int n, int m)  {
+	//se m è maggiore di n lo riduciamo a n perchè non 
+	//possiamo prendere m caratteri se c'è ne sono solo n
+	if(m > n){
+		m = n;
 	}
-	//differenza degli indice j e i + 2 per avere lo spazioe per il carattere \0
-	int len = j-i+2; 
-	char* newStr = malloc(len*sizeof(char));
-	int k = 0; //riferimento stringa newStr
-	while(j>=i){
-		newStr[k] = s[j];
+	//lo spazio sono m caratteri +1 per lo spazio di '\0'
+	char* result = (char*)malloc((m+1)*sizeof(char));
+	int k = 0; //indice stringa result
+	while(k<m){
+		result[k] = s[n-1-k];
 		k++;
-		j--;
 	}
-	newStr[k] = '\0';
-	return newStr;
+	result[k] = '\0';
+	return result;
 }
-
-int main(){
 	
-	char* s = "eccocosafare";
-	char* conversion = extract(s, 4, 7);
-	
-	printf("result:  '%s'\n", conversion);
-	printf("result lenght:  %d\n", strlen(conversion));
-	free(conversion);
+int main() {
+	char *str = "eccocosafare";
+	int len  = 12;
+	int val  = 5;
+	char *result = tailextract(str,len,val);
+	printf ("result - '%s'", result);
+	free(result);
+	return 0;	
 } 
