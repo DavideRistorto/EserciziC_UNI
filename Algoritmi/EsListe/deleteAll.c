dcd68182506d7d974043100a165565003b0f8730
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2025-01-04 14:48:49 +0100

    Update deleteAll.c

--- Algoritmi/EsListe/deleteAll.c
+++ Algoritmi/EsListe/deleteAll.c
@@ -1,29 +1,30 @@
 // post: ritorna la lista as da cui sono state rimosse 
 //       tutte le occorrenze di n
 list deleteAll(int n, list as) {
+    
     list prev = NULL;
     list current = as;
     
     while (current){
         //caso di cancellazione
         if(current->info == n){
+            //se elimino il pirmo nodo della lista, devo aggiornare il riferimento alla testa
             if(prev == NULL){
-                //aggiorno la testa della cosa
                 as = current->next;
-            }else{
-                //salto l' elemento
+            }
+            //se elimino un nodo, il tuo predecessore devo puntare al suo successore
+            else{
                 prev->next = current->next;
             }
+            //deallocazione
             list tmp = current;
             current = current->next;
             free(tmp);
-        }
+        }//caso in cui non cancello. itero su prev e current
         else{
-            //aggiorno i riferimenti di prev e current
             prev = current;
             current = current->next;
         }
-
     }
     return as;
 }
