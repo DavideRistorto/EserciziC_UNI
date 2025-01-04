7b03c517c91f09f4ba52dea75080e5d2ed934c15
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2025-01-04 14:40:44 +0100

    Update revese.c

--- Algoritmi/EsListe/revese.c
+++ Algoritmi/EsListe/revese.c
@@ -1,7 +1,10 @@
 list reverse_aux(list l, list acc) {
     if (l == NULL)
         return acc;
-    return reverse_aux(l->next, Cons(l->info, acc));
+    //aggiungo il nodo in testa lista [1,2,3] -> [1] -> [2,3] -> [3,2,1]
+    acc = Cons(l->info, acc);
+    //itero sul nodo successivo
+    return reverse_aux(l->next, acc);
 }
 
 // post: inverte non distruttivamente l in tempo 
