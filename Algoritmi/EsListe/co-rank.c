774a94f24861253df860dee1dfe717ecdb316fb0
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2025-01-04 14:30:57 +0100

    Update co-rank.c

--- Algoritmi/EsListe/co-rank.c
+++ Algoritmi/EsListe/co-rank.c
@@ -1,16 +1,7 @@
-//funzione che raccoglio il valore della somma
-// e nodo dopo nodo genera i coranghi
-list corank_rec(list l, int sum){
-    if (l == NULL){
-        return 0;
-    }
-    sum += l->info;
-    l->info = sum;
-    return corank_rec(l->next, sum);
-}
-
-
 // post: ogni nodo di l contiene il suo rango
-list corank(list l) {
-    return corank_rec(l, 0);
+int corank(list l) {
+    if(l == NULL || l->next == NULL) return 0;
+    
+    l->next->info += l->info;
+    return (corank(l->next));
 }
