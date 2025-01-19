10461fad83f2734d33629e965119de3934426c52
Davide Ristorto <ristorto.davide@gmail.com>
2025-01-19 16:17:59 +0100

    update

--- Algoritmi/EsAlberi/sumLeaf.c
+++ Algoritmi/EsAlberi/sumLeaf.c
@@ -1,14 +1,14 @@
 // post: ritorna la somma delle etichette delle foglie di t
-int sumLeaf (kTree t) {
-    if(t == NULL) return 0;
-    //se è una foglia, ritorno il suo valore
-    if(t->child == NULL) return t->key;
-    
-    kTree figlio = t->child;
-    int sum = 0;
-    while(figlio){
-        sum += sumLeaf(figlio);
-        figlio = figlio->sibling;
-    }
-    return sum;
+int sumLeaf(kTree t) {
+  if (t == NULL) return 0;
+  // se è una foglia, ritorno il suo valore
+  if (t->child == NULL) return t->key;
+
+  kTree current = t->child;
+  int sum = 0;
+  while (current) {
+    sum += sumLeaf(current);
+    current = current->sibling;
+  }
+  return sum;
 }
