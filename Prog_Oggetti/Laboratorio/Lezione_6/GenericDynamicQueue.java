867e04751ffc9d1a1780fd2fb6e8239cda506a66
Davide Ristorto <ristorto.davide@gmail.com>
2025-06-01 10:57:16 +0200

    rifatti

--- Prog_Oggetti/Laboratorio/Lezione_6/GenericDynamicQueue.java
+++ Prog_Oggetti/Laboratorio/Lezione_6/GenericDynamicQueue.java
@@ -1,109 +1,74 @@
 package Lezione_6;
 
-public class GenericDynamicQueue<T> {
-
-  private GenericNode<T> first;
-  private GenericNode<T> last;
-
-  public GenericDynamicQueue() {
-    first = null;
-    last = null;
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    GenericNode<T> current = first;
-    sb.append("[");
-    while (current != null) {
-      sb.append(current.getElem());
-      if (current.getNext() != null) {
-        sb.append(", ");
-      }
-      current = current.getNext();
-    }
-    sb.append("]");
-    return sb.toString();
-  }
-
-  public void enqueue(T x) {
-    GenericNode<T> node = new GenericNode<>(x);
-    // caso coda vuota
-    if (last == null) {
-      last = node;
-      first = node;
-      return;
-    }
-    last.setNext(node);
-    last = node;
-  }
-
-  public T dequeue() {
-    if (first == null) {
-      throw new IllegalStateException("Queue is empty");
-    }
-    GenericNode<T> current = first;
-    first = first.getNext();
-    return current.getElem();
-  }
-
-  public int size() {
-    int count = 0;
-    GenericNode<T> cur = first;
-    while (cur != null) {
-      count++;
-      cur = cur.getNext();
-    }
-    return count;
-  }
-
-  public boolean empty() {
-    return first == null;
-  }
-
-  public boolean contains(T x) {
-    // caso coda vuota
-    if (first == null) {
-      return false;
-    }
-    GenericNode<T> cur = first;
-    while (cur != null) {
-      if (cur.getElem().equals(x)) {
-        return true;
-      }
-      cur = cur.getNext();
-    }
-    return false;
-  }
-
-  public T front() {
-    if (first == null) {
-      throw new IllegalStateException("Queue is empty");
-    }
-    return first.getElem();
-  }
-
-  public static void main(String[] args) {
-    GenericDynamicQueue<Integer> queue = new GenericDynamicQueue<>();
-    System.out.println("Testing GenericDynamicQueue with Integer type:");
-
-    queue.enqueue(10);
-    queue.enqueue(20);
-    queue.enqueue(30);
-
-    System.out.println("Queue after enqueuing 10, 20, 30: " + queue);
-    System.out.println("Queue size: " + queue.size());
-    System.out.println("Queue front: " + queue.front());
-
-    System.out.println("Dequeue: " + queue.dequeue());
-    System.out.println("Queue after dequeue: " + queue);
-
-    System.out.println("Contains 20: " + queue.contains(20));
-    System.out.println("Contains 40: " + queue.contains(40));
-
-    System.out.println("Dequeue: " + queue.dequeue());
-    System.out.println("Dequeue: " + queue.dequeue());
-
-    System.out.println("Queue empty: " + queue.empty());
-  }
+public class GenericDynamicQueue <T> {
+	
+	private GenericNode<T> first;
+	private GenericNode<T> last;
+	
+	public GenericDynamicQueue(){
+		first = null;
+		last = null;
+	}
+	
+	@Override
+	public String toString(){
+		String s = "";
+		GenericNode<T> cur = first;
+		while(cur != null){
+			s += cur.getElem() + " ";
+			cur = cur.getNext();
+		}
+		return s;
+	}
+	
+	public void enqueue(GenericNode<T> n){
+		//coda vuota
+		if(first == null){
+			first = n;
+			last = first;
+		}else{
+			//metto il nodo alla fine e diventa il nuovo last
+			last.setNext(n);
+			last = n;
+		}
+	}
+	
+	public T dequeue(){
+		if(first == null) return null;
+		//salvo il valore e shifto il first
+		T val = first.getElem();
+		first = first.getNext();
+		return val;
+	}
+	
+	public int size(){
+		int counter = 0;
+		GenericNode<T> c = first;
+		while(c != null){
+			counter++;
+			c = c.getNext();
+		}
+		return counter;
+	}
+	
+	public T front(){
+		return first != null ? first.getElem() :  null;
+	}
+	
+	public boolean isEmpty(){
+		return first == null;
+	}
+	
+	public boolean contains(int x){
+		if(first == null) return false;
+		
+		GenericNode<T> c = first;
+		while(c != null){
+			if(c.getElem().equals(x)){
+				return true;
+			}
+			c = c.getNext();
+		}
+		return false;
+	}
 }
--- Prog_Oggetti/Laboratorio/Lezione_6/GenericNode.java
+++ Prog_Oggetti/Laboratorio/Lezione_6/GenericNode.java
@@ -1,28 +1,18 @@
 package Lezione_6;
 
-public class GenericNode<T> {
+public class GenericNode <T> {
+	
+	private T elem;
+	private GenericNode<T> next;
+	
+	public GenericNode(T e, GenericNode<T> n){
+		elem = e;
+		next = n;
+	}
+	
+	public T getElem(){return elem;}
+	public GenericNode<T> getNext(){return next;}
+	public void setElem(T e){elem = e;}
+	public void setNext(GenericNode<T> n){next = n;}
 
-  private T elem;
-  private GenericNode<T> next;
-
-  public GenericNode(T elem){
-    this.elem = elem;
-    this.next = null;
-  }
-
-  public T getElem(){
-    return this.elem;
-  }
-
-  public GenericNode<T> getNext(){
-    return this.next;
-  }
-
-  public void setElem(T elem){
-    this.elem = elem;
-  }
-
-  public void setNext(GenericNode<T> next){
-    this.next = next;
-  }
 }
