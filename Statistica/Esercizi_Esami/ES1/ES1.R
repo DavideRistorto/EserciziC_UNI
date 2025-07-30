80e3f18003308c3487fde348c2202349d8ec4d9f
Davide Ristorto <ristorto.davide@gmail.com>
2025-07-30 21:38:23 +0200

    Update ES1.R

--- Statistica/Esercizi_Esami/ES1/ES1.R
+++ Statistica/Esercizi_Esami/ES1/ES1.R
@@ -1,24 +1,23 @@
-# Carica il dataset (assicurati di essere nella directory corretta)
-load("ricerca.RData")
-# Visualizza la struttura dei dati per capire cosa contiene
-str(dati)   # Mostra struttura del dataset
-head(dati)  # Mostra le prime righe
+setwd("C:/Users/Davide/Desktop/Scuola/EserciziC_UNI/Statistica/Esercizi_Esami/ES1")
 
-table(dati$algoritmo) #calcolo frequenza assoluta di una colonna, in questo caso il numero di volte in cui sono stati usati gli algoritmi
+load("ricerca.RData") # carico il data-set
+str(dati)             # descrizione compatta delle colonne dei dati -> colonna algoritmo e tempo
+head(dati)            # visualizzo le prime righe dei dati
+table(dati$algoritmo) #calcolo la frequenza per i valori distinti di una colonna
+mean(dati$tempo[dati$algoritmo == 'alg_A'])   # calcolo la media dei tempi solo degli algoritmi di tipo A
+sd(dati$tempo[dati$algoritmo == 'alg_A'])     # calcolo la deviazione standard dei tempi solo degli algoritmi di tipo A
+quantile(dati$tempo[dati$algoritmo == 'alg_A'], 0.95) #calcolo il 5% degli algoritmi più lenti, ovvero il 95 percentile
 
-mean(dati$tempo[dati$algoritmo == "alg_A"]) #media dei valori di tempo, filtrati solo per l' algoritmo A
-sd(dati$tempo[dati$algoritmo == "alg_A"])   #deviazione standard dei valori di tempo, filtrati solo per l' algoritmo A
+#intervallo di confidenza per la stima delle medie dei vari algoritmi
+t.test(dati$tempo[dati$algoritmo == 'alg_A'], conf.level = 0.99)  
+t.test(dati$tempo[dati$algoritmo == 'alg_B'], conf.level = 0.99)
 
-quantile(dati$tempo[dati$algoritmo == "alg_A"], 0.95) #Il 5% dei tempi di risposta più lenti dell'algoritmo A. Questo valore è il 95° percentile della distribuzione
+# i valori degli intervalli si intrecciano, quindi è possibile che abbiano la stessa media
 
-
+# grafico
 boxplot(tempo ~ algoritmo, 
         data = dati,
         main = "Confronto Tempi di Risposta: alg_A vs alg_B",
         xlab = "Algoritmo",
         ylab = "Tempo (ms)",
         col = c("lightblue", "salmon"))
-
-#Calcolo due intervalli di confidenza e li confronto.
-t.test(dati$tempo[dati$algoritmo == "alg_A"], conf.level = 0.99) #Specifica che l'intervallo di confidenza deve essere calcolato al 99%
-t.test(dati$tempo[dati$algoritmo == "alg_B"], conf.level = 0.99)
