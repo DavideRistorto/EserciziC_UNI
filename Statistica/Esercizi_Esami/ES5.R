474342da9a31815c039479b7d8e06c2f522c495f
Davide Ristorto <ristorto.davide@gmail.com>
2025-08-02 15:49:37 +0200

    Create ES5.R

---
+++ Statistica/Esercizi_Esami/ES5.R
@@ -0,0 +1,14 @@
+#Procedi con il seguente esperimento: lanci una moneta truccata che restituisce testa con probabilità 0.3. Se ottieni testa estrai con reimbussolamento 8 palline da un’urna che ne contiene 11, delle quali 4 sono bianche e 7 sono verdi. 
+#Se ottieni croce estrai, con reimbussolamento dalla stessa urna, 6 palline
+
+#Con quale probabilità ottieni 3 palline verdi
+dbinom(3, 8, 7/11) * 0.3 + dbinom(3, 6, 7/11) * 0.7
+
+#Con quale probabilità ottieni 7 palline verdi
+dbinom(7, 8, 7/11) * 0.3 + dbinom(7, 6, 7/11) * 0.7
+
+#Con quale probabilità ottieni meno di (<) 3 palline verdi?
+(dbinom(0, 8, 7/11) + dbinom(1, 8, 7/11) + dbinom(2, 8, 7/11)) * 0.3 + (dbinom(0, 6, 7/11) + dbinom(1, 6, 7/11) + dbinom(2, 6, 7/11)) * 0.7
+
+#Con quale probabilità avevi ottenuto testa se hai estratto meno di (<) 3 palline verdi?
+#?????????????????????????????????
