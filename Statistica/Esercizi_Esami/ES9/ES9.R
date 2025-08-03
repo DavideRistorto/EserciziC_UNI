e936db22110499f89c03117d4ce88333d2197551
Davide Ristorto <ristorto.davide@gmail.com>
2025-08-03 11:48:03 +0200

    es 9

---
+++ Statistica/Esercizi_Esami/ES9/ES9.R
@@ -0,0 +1,19 @@
+setwd("C:/Users/Davide/Desktop/Scuola/EserciziC_UNI/Statistica/Esercizi_Esami/ES9")
+
+load("applicazione.RData")
+str(dati)
+head(dati)
+
+nrow(dati)
+sum(is.na(dati))
+
+mean(dati$time_new_version)
+sd(dati$time_new_version)
+
+sum(dati$time_new_version > 26.5)
+
+quantile(dati$time_new_version, 0.9)
+
+hist(dati$time_old_version)
+
+t.test(dati$time_new_version, dati$time_old_version, alternative = "less", paired = TRUE)
