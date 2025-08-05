eacc89c59af512e84949294f1b27c01600df76c0
Davide Ristorto <ristorto.davide@gmail.com>
2025-08-05 17:25:21 +0200

    Update ES16.R

---
+++ Statistica/Esercizi_Esami/ES16.R
@@ -0,0 +1,38 @@
+#Sia X una variabile aleatoria distribuita come una Normale di media 0.2e varianza 1.
+#Sia Y una variabile aleatoria distribuita come una Esponenziale di parametro ('rate') pari a 1, indipendente da  X
+
+#La probabilità che Y sia maggiore di 2
+pexp(2, 1, lower.tail = FALSE)
+
+#La probabilità che almeno una delle due variabili aleatorie sia maggiore di 2
+1 - (pnorm(2, 0.2, 1) * pexp(2, 1))  # 1 - la probabilità che entrambe siano minore di 2
+
+#La probabilità che al massimo una delle due sia maggiore di 1
+1 - (pnorm(1,0.2,1, , lower.tail = FALSE) * (pexp(1,1, lower.tail = FALSE)))
+
+
+
+#Siano X1,X2,X3 tre variabili aleatorie indipendenti e identicamente distribuite, 
+#con distribuzione Normale di media 0.2 e varianza 1
+
+# La probabilità che X2 sia maggiore di 2
+pnorm(2, 0.2, 1, lower.tail = FALSE)
+
+#La probabilità che almeno una delle tre variabili aleatorie sia maggiore di 1
+1 - (pnorm(1, 0.2, 1) ^ 3)
+
+#La probabilità che Y=2X − 0.4 sia maggiore di 1
+pnorm(0.7, 0.2, 1, lower.tail = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
