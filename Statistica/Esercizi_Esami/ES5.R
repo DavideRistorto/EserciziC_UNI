e5f47522ee62b150b3fe648b8472617c719869c9
Davide Ristorto <ristorto.davide@gmail.com>
2025-08-08 11:20:54 +0200

    Update ES5.R

--- Statistica/Esercizi_Esami/ES5.R
+++ Statistica/Esercizi_Esami/ES5.R
@@ -27,6 +27,45 @@
 (4^2 + 5^2 + 6^2)/(15^2)
 
 
+#L’urna A contiene 10 palline bianche e 10 nere. L’urna B contiene 5 palline bianche e 15 nere. Viene lanciata una moneta non truccata: se esce testa allora vengono fatte 
+# 3 estrazioni senza reinserimento dall’urna A; se esce croce vengono fatte 5 estrazioni con reinserimento dall’urna B
+
+#Calcolare la probabilità di estrarre esattamente 2 palline bianche sapendo di aver ottenuto testa
+(choose(10, 2) * choose(10, 1)) / choose(20, 3)
+
+#Calcolare la probabilità di estrarre esattamente 2 palline bianche sapendo di aver ottenuto croce
+dbinom(2,5,5/20)
+
+
+#Si hanno a disposizione 3 monete. La moneta A ha probabilità di ottenere testa pari a p
+#La moneta B ha probabilità di ottenere testa pari a p/2
+#La moneta C ha probabilità di ottenere testa pari a p/3
+#Primo lancio: Si lancia la moneta A.
+#Secondo lancio: Se al lancio precedente si è ottenuto testa allora si lancia la moneta A. Se si è avuto croce si lancia la moneta B.
+#Terzo lancio: Se nel lancio precedente si è ottenuto testa allora si lancia la moneta A. Se nel primo lancio si è ottenuto testa e nel secondo lancio si è ottenuto croce, allora si lancia la moneta B. Se nel primo e nel secondo lancio si è ottenuto croce allora si lancia la moneta C
+
+
+
+#Se p=1/4, determinare la probabilità della sequenza (Testa,Testa,Testa) 
+1/4 ^ 3
+
+# Se p=1/2 determinare la probabilità della sequenza (Croce, Croce, Croce)
+1/2 * 3/4 * 5/6
+
+#Se p=1/4, determinare la probabilità che si abbia Testa al secondo e al terzo lancio
+1/4 ^ 3 + 3/4 * 1/8 * 1/4
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
