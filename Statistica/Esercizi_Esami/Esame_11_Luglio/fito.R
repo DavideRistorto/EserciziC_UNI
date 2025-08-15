87602d5bf745fcc2d298b0816bba599e5b7838e1
Davide Ristorto <ristorto.davide@gmail.com>
2025-08-15 11:08:01 +0200

    Update fito.R

--- Statistica/Esercizi_Esami/Esame_11_Luglio/fito.R
+++ Statistica/Esercizi_Esami/Esame_11_Luglio/fito.R
@@ -1,3 +1,5 @@
+################################# ES1
+
 setwd("C:/Users/Davide/Desktop/Scuola/EserciziC_UNI/Statistica/Esercizi_Esami/Esame_11_Luglio")
 
 load("fito.RData")
@@ -18,3 +20,34 @@ plot(fito$temperatura,fito$fitoplancton)
 cor.test(fito$temperatura, fito$fitoplancton)
 
 summary(lm(fitoplancton ~ temperatura, data = dati))
+
+################################# ES2
+pnorm(1.5, 0, 2)
+pnorm(3, 0, 2, lower.tail = FALSE)
+(pnorm(6, 0, 2) - pnorm(3, 0, 2)) / pnorm(6, 0, 2)
+
+################################# ES3
+
+load("escursioni.RData")
+head(escursioni)
+str(escursioni)
+
+nrow(escursioni)
+table(escursioni$Preferenza)
+proportions(table(escursioni$Preferenza))
+
+mean(escursioni$`Numero escursioni`[escursioni$Preferenza == "Sentiero facile"])
+
+barplot(table(escursioni$Preferenza))
+hist(escursioni$`Numero escursioni`)
+
+binom.test(91, 162, alternative = "greater", p = 0.5)
+
+
+
+
+
+
+
+
+
