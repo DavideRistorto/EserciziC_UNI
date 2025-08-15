6b06c8f2fa512718a27549791378035237efb20d
Davide Ristorto <ristorto.davide@gmail.com>
2025-08-15 13:51:05 +0200

    esame

---
+++ Statistica/Esercizi_Esami/Esame_29_Gennaio/alberi.R
@@ -0,0 +1,54 @@
+################################# ES1
+
+setwd("C:/Users/Davide/Desktop/Scuola/EserciziC_UNI/Statistica/Esercizi_Esami/Esame_29_Gennaio")
+
+load("alberi.RData")
+head(dati)
+
+nrow(dati)
+sum(is.na(dati))
+dati <- na.omit(dati)
+
+mean(dati$diametro[dati$altezza >= 25 & dati$altezza <= 35])
+sd(dati$diametro[dati$altezza >= 25 & dati$altezza <= 35])
+
+quantile(dati$altezza, 0.9)
+
+boxplot(dati$altezza[dati$diametro < 40], dati$altezza[dati$diametro >= 40])
+
+cor.test(dati$altezza, dati$diametro)
+
+lm(altezza ~ diametro, data = dati)
+
+################################# ES2
+
+pexp(1.5, 2) + pexp(3, 2, lower.tail = FALSE)
+pexp(3, 2, lower.tail = FALSE)
+pexp(6,2) - pexp(3,2)
+
+################################# ES3
+options(digits = 10)
+load("fertilizzante.RData")
+head(dati)
+table(dati$fertilizzante)
+mean(dati$altezza[dati$fertilizzante == "A"])
+sd(dati$altezza[dati$fertilizzante == "A"])
+
+quantile(dati$altezza[dati$fertilizzante == "A"], 0.9)
+
+boxplot(dati$altezza[dati$fertilizzante == "A"], dati$altezza[dati$fertilizzante == "B"])
+t.test(dati$altezza[dati$fertilizzante == "A"], dati$altezza[dati$fertilizzante == "B"], paired = FALSE, alternative = "two.sided")
+
+
+################################# ES4
+
+#Si tira quattro volte una moneta non equa, con probabilità di ottenere testa pari a 1/3
+
+#i primi tre lanci sono uguali
+(2/3)^3 + (1/3)^3
+
+#gli ultimi due risultati sono uguali
+(2/3)^2 + (1/3)^2
+
+# Sia A che B
+(2/3)^4 + (1/3)^4
