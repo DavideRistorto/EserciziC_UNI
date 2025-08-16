c083bbdaee98f991301664f5e191dcbb8a3a2c05
Davide Ristorto <ristorto.davide@gmail.com>
2025-08-16 21:06:58 +0200

    gennaio

---
+++ Statistica/Esercizi_Esami/Esame_29_Gennaio/nidi.R
@@ -0,0 +1,42 @@
+setwd("C:/Users/Davide/Desktop/Scuola/EserciziC_UNI/Statistica/Esercizi_Esami/Esame_29_Gennaio")
+
+################################# ES1
+
+load("nidi.RData")
+head(dati)
+
+nrow(dati)
+which(is.na(dati),arr.ind = TRUE) #per capire dove è il dato nullo
+table(dati$Foresta)
+proportions(table(dati$Foresta))
+dati <- na.omit(dati)
+sum(dati$Foresta == "Primaria" & dati$Nido == "Occupato")
+
+#barplot
+barplot(table(dati$Nido))
+barplot(table(dati$Foresta))
+
+min(chisq.test(table(dati))$expected)
+
+################################# ES2
+pnorm(3, 1,1, lower.tail = FALSE)
+(pnorm(6, 1, 1) - pnorm(0.5, 1, 1)) / pnorm(6, 1, 1)
+
+################################# ES3
+dpois(2, 10) + dpois(7, 10) + dpois(8, 10) + dpois(9, 10)
+ppois(4, 10, lower.tail = FALSE)
+(ppois(3, 10) - ppois(0, 10)) / (1 - dpois(0, 10))
+
+################################# ES4
+
+load("pallavolo.RData")
+head(dati)
+
+nrow(dati)
+mean(dati$DOPO)
+sd(dati$DOPO)
+sum(dati$DOPO > 0.7)
+quantile(dati$DOPO, 0.95)
+
+boxplot(dati$PRIMA, dati$DOPO)
+t.test(dati$PRIMA, dati$DOPO, paired = TRUE, alternative = "less")
