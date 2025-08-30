d873f54b01ac4e55c282fe46dc84d38e8f8c6eb2
Davide Ristorto <ristorto.davide@gmail.com>
2025-08-30 11:09:11 +0200

    Update nidi.R

--- Statistica/Esercizi_Esami/Esame_29_Gennaio/nidi.R
+++ Statistica/Esercizi_Esami/Esame_29_Gennaio/nidi.R
@@ -6,26 +6,30 @@ load("nidi.RData")
 head(dati)
 
 nrow(dati)
-which(is.na(dati),arr.ind = TRUE) #per capire dove è il dato nullo
+which(is.na(dati$Nido))
+dati <- na.omit(dati)
+
 table(dati$Foresta)
 proportions(table(dati$Foresta))
-dati <- na.omit(dati)
 sum(dati$Foresta == "Primaria" & dati$Nido == "Occupato")
 
-#barplot
 barplot(table(dati$Nido))
 barplot(table(dati$Foresta))
 
-min(chisq.test(table(dati))$expected)
+min(chisq.test(dati$Foresta, dati$Nido)$expected)
+
 
 ################################# ES2
-pnorm(3, 1,1, lower.tail = FALSE)
-(pnorm(6, 1, 1) - pnorm(0.5, 1, 1)) / pnorm(6, 1, 1)
+
+pnorm(3, 1, 1, lower.tail = FALSE)
+(pnorm(6, 1, 1) - pnorm(0.5, 1, 1))/pnorm(6, 1, 1)
 
 ################################# ES3
-dpois(2, 10) + dpois(7, 10) + dpois(8, 10) + dpois(9, 10)
+
+dpois(2, 10) + dpois(7,10) + dpois(8, 10) + dpois(9, 10)
 ppois(4, 10, lower.tail = FALSE)
-(ppois(3, 10) - ppois(0, 10)) / (1 - dpois(0, 10))
+(ppois(3, 10) - ppois(0, 10) ) / (1-dpois(0,10))
+
 
 ################################# ES4
 
@@ -35,6 +39,7 @@ head(dati)
 nrow(dati)
 mean(dati$DOPO)
 sd(dati$DOPO)
+
 sum(dati$DOPO > 0.7)
 quantile(dati$DOPO, 0.95)
 
