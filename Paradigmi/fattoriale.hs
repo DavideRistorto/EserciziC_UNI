27be9f13b349d58b3cecce732882315b668dde1c
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2025-10-05 12:03:49 +0200

    Update fattoriale.hs

--- Paradigmi/fattoriale.hs
+++ Paradigmi/fattoriale.hs
@@ -2,6 +2,11 @@ fattoriale :: Int -> Int
 fattoriale n | n == 0 = 1
              | otherwise = n * fattoriale (n-1)
 
+--Tecnica del pattern matching (riconosco l' input e scelgo la definizione giusta)
+fattoriale1:: Int -> Int
+fattoriale1 0 = 1
+fattoriale1 n = n * fattoriale1 (n-1)
+
 main :: IO ()
 main = do
   print (fattoriale 1)
