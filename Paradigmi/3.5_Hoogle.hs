82086bdfd68696ff9de55ac4386a3003c2078cfe
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2025-10-23 18:48:50 +0200

    Create 3.5_Hoogle.hs

---
+++ Paradigmi/3.5_Hoogle.hs
@@ -0,0 +1,20 @@
+{-
+Hoogle
+==================================
+
+Hoogle è un motore di ricerca per le API Haskell. 
+Ti permette di cercare tra le librerie Haskell su Stackage usando il nome di una funzione oppure approssimativamente
+la sua firma di tipo. 
+Ad esempio, puoi cercare funzioni come map, firme di tipo come (a -> b) -> [a] -> [b], 
+oppure anche funzioni specifiche di librerie come Data.Set.insert.
+
+-- Esempi di utilizzo di take e drop in Haskell
+
+-- take n xs prende i primi n elementi della lista xs
+-- Esempio:
+-- take 3 [1,2,3,4,5] == [1,2,3]
+
+-- drop n xs elimina i primi n elementi della lista xs
+-- Esempio:
+-- drop 3 [1,2,3,4,5] == [4,5]
+-}
