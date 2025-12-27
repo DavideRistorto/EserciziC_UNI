8de8543b3e78da70b1412f075e210ad3db4f11bc
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2025-12-27 19:05:33 +0100

    Update 2.1_FunzioniConGuardie.hs

--- Paradigmi/2.1_FunzioniConGuardie.hs
+++ Paradigmi/2.1_FunzioniConGuardie.hs
@@ -30,22 +30,20 @@ La funzione `otherwise` è sempre vera e può essere usata come ultima guardia e
 -}
 
 -- Esercizi di riepilogo capitolo
-
 assoluto :: Int -> Int
 assoluto n | n >= 0 = n
-           | n < 0  = negate n  -- oppure | otherwise = negate n 
-
+           | otherwise = negate n
 -- 1) Definire una funzione Int -> Int che, data n, calcola il successore se n è pari e il valore assoluto se n è dispari. (Due versioni: prima con if, poi con guardie)
-funzione_v1 :: Int -> Int
-funzione_v1 n | even n = n + 1
-              | otherwise = abs n
+funzione :: Int -> Int
+funzione n = if even n then n + 1 else abs n
 
 funzione_v2 :: Int -> Int
-funzione_v2 n = if even n then n + 1 else abs n
+funzione_v2 n | even n = n + 1
+              | otherwise = abs n
 
 -- 2) Definire giorni :: Int -> Int, che calcola il numero di giorni di un anno a seconda che sia bisestile o no (usando la funzione bisestile vista precedentemente).
 bisestile :: Int -> Bool
-bisestile n = n `mod` 4 == 0 && n `mod` 100 /= 0
+bisestile x = x `mod` 4 == 0 && x `mod` 100 /= 0 && x `mod` 400 == 0
 
 giorni :: Int -> Int
-giorni anno = if bisestile anno then 366 else 365
+giorni n = if bisestile n then 366 else 365
