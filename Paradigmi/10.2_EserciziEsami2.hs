5582587023050a6c251e0345c4947f94ec9f7f85
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2026-01-14 21:00:55 +0100

    Update 10.2_EserciziEsami2.hs

--- Paradigmi/10.2_EserciziEsami2.hs
+++ Paradigmi/10.2_EserciziEsami2.hs
@@ -31,3 +31,32 @@ diverso_ric (x:xs) ys = aux x ys || diverso_ric xs ys
     where
         aux x [] = True
         aux x (y:ys) = x /= y && aux x ys
+
+diverso_lib :: Eq a => [a] -> [a] -> Bool
+diverso_lib xs ys = any (\x -> not (elem x ys)) xs 
+
+-- Scrivere una funzione che trovi il primo numero negativo in una lista di numeri interi
+primo_negativo_ric :: (Num a, Ord a) => [a] -> Maybe a
+primo_negativo_ric [] = Nothing
+primo_negativo_ric (x:xs) = if x < 0 then (Just x) else primo_negativo_ric xs 
+
+--Scrivere una funzione che calcoli la somma alternata degli elementi di una lista (primo elemento con segno +, secondo con segno -, terzo con segno +
+somma_alternata_ric :: Num a => [a] -> a
+somma_alternata_ric [] = 0
+somma_alternata_ric [x] = x
+somma_alternata_ric (x:y:xs) = x - y + somma_alternata_ric xs
+
+somma_alternata_lib :: Num a => [a] -> a
+somma_alternata_lib xs = sum (map (\(a,b) -> if odd b then -a else a )(zip xs [0..]))
+
+ultimo_multiplo_lib :: Integral a => a -> [a] -> a
+ultimo_multiplo_lib a xs = head (filter (\x -> x `mod`  a == 0)(reverse xs))
+
+ultimo_multiplo_ric :: Integral a => a -> [a] -> Maybe a
+ultimo_multiplo_ric a xs = aux Nothing a xs
+    where 
+        aux ultimo _ [] = ultimo
+        aux ultimo a (x:xs) = if x `mod` a == 0 then aux (Just x) a xs else aux ultimo a xs
+
+conta_palindromi :: [String] -> Int
+conta_palindromi (str:xs) = if (str == (reverse str)) then 1 + conta_palindromi xs else conta_palindromi xs
