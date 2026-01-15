aef13c8c1cacdea45659eada790a872b643b649d
Davide Ristorto <91005671+DavideRistorto@users.noreply.github.com>
2026-01-15 18:41:02 +0100

    alberi

---
+++ Paradigmi/10.3_EserciziAlberi.hs
@@ -0,0 +1,9 @@
+data Tree a = Leaf | Branch a (Tree a) (Tree a) deriving Show
+
+height :: Tree a -> Int
+height Leaf = 0
+height (Branch x sx dx) = 1 + max (height sx) (height dx)
+
+size :: Tree a -> Int
+size Leaf = 0
+size (Branch x sx dx) = 1 + size sx + size dx 
